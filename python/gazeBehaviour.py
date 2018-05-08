5d6a4dc022871fbb1f959644f9862db04f558710
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-05-08 15:45:26 +0100

    run all code in one thread; sending signal to yarp port with specific color

--- python/gazeBehaviour.py
+++ python/gazeBehaviour.py
@@ -6,42 +6,43 @@ class gazeBehaviour:
     def __init__(self, outlet=None):
         pass
 
-
-    def record(self, timestamp, markers, ball, faces, fixation, labels):
+    def record(self, timestamp, markers, allBalls, faces, fixation, labels):
         epsilon = 30  # the threshold in pixels allowed
 
-        distX = fixation[0] - ball[0][0]
-        distY = fixation[1] - ball[0][1]
-
-        if math.sqrt(pow(distX, 2) + pow(distY, 2)) < epsilon:
-            mysample = [timestamp, 0.0, ball[0][0], ball[0][1]]
-            return mysample
-
-        for marker in markers:
-
-            cX = marker[0]
-            cY = marker[1]
-
-            distX = fixation[0] - cX
-            distY = fixation[1] - cY
+        for ball in allBalls:
+            distX = fixation[0] - ball[0][0][0]
+            distY = fixation[1] - ball[0][0][1]
 
             if math.sqrt(pow(distX, 2) + pow(distY, 2)) < epsilon:
-                mysample = [timestamp, 1.0, marker[0], marker[1]]
+                #           time        ball    x           y           color of ball
+                mysample = [timestamp, 0.0, ball[0][0][0], ball[0][0][1], ball[1]]
                 return mysample
 
-        i = 0
-        for face in faces:
+            for marker in markers:
 
-            cX = face[0]
-            cY = face[1]
-            cW = face[0] + face[2]
-            cH = face[1] + face[3]
+                cX = marker[0]
+                cY = marker[1]
 
-            if cX - 30 < fixation[0] < cW + 30 and cY - 30 < fixation[1] < cH + 30:
-                mysample = [timestamp, 1+i, face[0], face[1]]
-                return mysample
+                distX = fixation[0] - cX
+                distY = fixation[1] - cY
 
-            i = i+1
-        mysample = [timestamp, 4.0, 0.0, 0.0]
+                if math.sqrt(pow(distX, 2) + pow(distY, 2)) < epsilon:
+                    mysample = [timestamp, 1.0, marker[0], marker[1]]
+                    return mysample
+
+            i = 0
+            for face in faces:
+
+                cX = face[0]
+                cY = face[1]
+                cW = face[0] + face[2]
+                cH = face[1] + face[3]
+
+                if cX - 30 < fixation[0] < cW + 30 and cY - 30 < fixation[1] < cH + 30:
+                    mysample = [timestamp, 1+i, face[0], face[1]]
+                    return mysample
+
+                i = i+1
+            mysample = [timestamp, 4.0, 0.0, 0.0]
         return mysample
 
--- python/onlineDetection.py
+++ python/onlineDetection.py
@@ -16,106 +16,86 @@ import csv
 import zmq
 import argparse
 import imutils
-import threading
 
 """
 Receive world camera data from Pupil using ZMQ.
 Make sure the frame publisher plugin is loaded and configuredsource  to gray or rgb
 """
 
-class myThread (threading.Thread):
+context = zmq.Context()
+# open a req port to talk to pupil
+addr = '127.0.0.1'  # remote ip or localhost
+req_port = "50020"  # same as in the pupil remote gui
+req = context.socket(zmq.REQ)
+req.connect("tcp://{}:{}".format(addr, req_port))
+# ask for the sub port
+req.send_string('SUB_PORT')
+sub_port = req.recv_string()
 
-    def __init__(self, threadID, name, args):
-        threading.Thread.__init__(self)
-        self.args = args
-        context = zmq.Context()
-        # open a req port to talk to pupil
-        addr = '127.0.0.1'  # remote ip or localhost
-        req_port = "50020"  # same as in the pupil remote gui
-        self.req = context.socket(zmq.REQ)
-        self.req.connect("tcp://{}:{}".format(addr, req_port))
-        # ask for the sub port
-        self.req.send_string('SUB_PORT')
-        sub_port = self.req.recv_string()
+# create a new stream info
+info = StreamInfo("GazePose", "NormPose2IP", 4, 100, "float32", "myuid34234")
+info.desc().append_child_value("manufacturer", "Vislab")
+outlet = StreamOutlet(info)
 
-        # create a new stream info
-        info = StreamInfo("GazePose", "NormPose2IP", 4, 100, "float32", "myuid34234")
-        info.desc().append_child_value("manufacturer", "Vislab")
-        self.outlet = StreamOutlet(info)
+# send notification:
+def notify(notification):
+    """Sends ``notification`` to Pupil Remote"""
+    topic = 'notify.' + notification['subject']
+    payload = packb(notification, use_bin_type=True)
+    req.send_string(topic, flags=zmq.SNDMORE)
+    req.send(payload)
+    return req.recv_string()
 
-        # Start frame publisher with format BGR
-        self.notify({'subject': 'start_plugin', 'name': 'Frame_Publisher', 'args': {'format': 'bgr'}})
+# Start frame publisher with format BGR
+notify({'subject': 'start_plugin', 'name': 'Frame_Publisher', 'args': {'format': 'bgr'}})
 
-        # open a sub port to listen to pupil
-        self.sub = context.socket(zmq.SUB)
-        self.sub.connect("tcp://{}:{}".format(addr, sub_port))
+# open a sub port to listen to pupil
+sub = context.socket(zmq.SUB)
+sub.connect("tcp://{}:{}".format(addr, sub_port))
 
-        # set subscriptions to topics
-        # recv just pupil/gaze/notifications
-        self.sub.setsockopt_string(zmq.SUBSCRIBE, 'frame.')
+# set subscriptions to topics
+# recv just pupil/gaze/notifications
+sub.setsockopt_string(zmq.SUBSCRIBE, 'frame.')
 
-        # construct the argument parse and parse the arguments
-        ap = argparse.ArgumentParser()
-        ap.add_argument("-v", "--video", help="path to the (optional) video file")
-        ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
-        args = vars(ap.parse_args())
-        pts = deque(maxlen=args["buffer"])
+def recv_from_sub():
+    '''Recv a message with topic, payload.
 
-        self.ballTracking = Ball()
+    Topic is a utf-8 encoded string. Returned as unicode object.
+    Payload is a msgpack serialized dict. Returned as a python dict.
 
-        self.gaze = gazeBehaviour(self.outlet)
+    Any addional message frames will be added as a list
+    in the payload dict with key: '__raw_data__' .
+    '''
+    topic = sub.recv_string()
+    payload = unpackb(sub.recv(), encoding='utf-8')
+    extra_frames = []
+    while sub.get(zmq.RCVMORE):
+        extra_frames.append(sub.recv())
+    if extra_frames:
+        payload['__raw_data__'] = extra_frames
+    return topic, payload
 
-        print("looking for an NormPose2IP stream...")
-        streams = resolve_stream('name', 'NormPose2IP')
-        # create a new inlet to read from the stream
-        self.inlet = StreamInlet(streams[0])
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
+args = vars(ap.parse_args())
+pts = deque(maxlen=args["buffer"])
 
-        # while cv2.waitKey(1):
-        # Define a function for the thread
-        self.i = 0
-        print(self.args)
+ballTracking = Ball()
 
-    def run(self):
-        print("Starting " + self.name)
-        while 1:
-            detection(self)
-        print("Exiting " + self.name)
+gaze = gazeBehaviour(outlet)
 
-    # send notification:
-    def notify(self, notification):
-        """Sends ``notification`` to Pupil Remote"""
-        topic = 'notify.' + notification['subject']
-        payload = packb(notification, use_bin_type=True)
-        self.req.send_string(topic, flags=zmq.SNDMORE)
-        self.req.send(payload)
-        return self.req.recv_string()
+print("looking for an NormPose2IP stream...")
+streams = resolve_stream('name', 'NormPose2IP')
+# create a new inlet to read from the stream
+inlet = StreamInlet(streams[0])
 
+i = 0
+while cv2.waitKey(1):
+    topic, msg = recv_from_sub()
 
-    def recv_from_sub(self, ):
-        '''Recv a message with topic, payload.
-
-        Topic is a utf-8 encoded string. Returned as unicode object.
-        Payload is a msgpack serialized dict. Returned as a python dict.
-
-        Any addional message frames will be added as a list
-        in the payload dict with key: '__raw_data__' .
-        '''
-        topic = self.sub.recv_string()
-        payload = unpackb(self.sub.recv(), encoding='utf-8')
-        extra_frames = []
-        while self.sub.get(zmq.RCVMORE):
-            extra_frames.append(self.sub.recv())
-        if extra_frames:
-            payload['__raw_data__'] = extra_frames
-        return topic, payload
-
-
-def detection(self, ):
-# while cv2.waitKey(1):
-
-    topic, msg = self.recv_from_sub()
-
-    if topic == 'frame.world' and self.i % 2 == 0:
+    if topic == 'frame.world' and i % 5 == 0:
         frame = np.frombuffer(msg['__raw_data__'][0], dtype=np.uint8).reshape(msg['height'], msg['width'], 3)
 
         # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -123,12 +103,15 @@ def detection(self, ):
             frame = imutils.resize(frame, width=750)
             height, width, channels = frame.shape
 
-            frame, pts, ball = self.ballTracking.tracking(frame, [], self.args)   # pts
+            frame, pts, ballG = ballTracking.trackingGreen(frame, pts, args)
+            frame, pts, ballR = ballTracking.trackingRed(frame, pts, args)
+            frame, pts, ballB = ballTracking.trackingBlue(frame, pts, args)
 
+            ball = [[ballG, 1.1], [ballB, 1.2], [ballR, 1.3]]
             # anterior, faces, facesTrained = face.detecting(frame, anterior, faceCascade)
             # labels = face.predict(frame, face_recognizer, faces, facesTrained)
 
-            sample, timestamp = self.inlet.pull_chunk()
+            sample, timestamp = inlet.pull_chunk()
             if sample:
                 pos_x = sample[0][1]
                 pos_y = sample[0][2]
@@ -141,23 +124,13 @@ def detection(self, ):
 
                 # check the gaze behaviour
                 if len(ball) is not 0:
-                    mysample = self.gaze.record(sample[0][0], [], ball, [], fixation, [])
+                    mysample = gaze.record(sample[0][0], [], ball, [], fixation, [])
                     if mysample is not 0:
                         #print(mysample)
-                        self.outlet.push_sample(mysample)
+                        outlet.push_sample(mysample)
 
         cv2.imshow('frame', frame)
-    self.i = self.i + 1
-
-
-# Create new threads
-thread1 = myThread(1, "Thread-1", "blue")
-# thread2 = myThread(2, "Thread-2", 2)
-
-# Start new Threads
-thread1.start()
-thread1.join()
-
+    i = i + 1
 
 # cap.release()
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
