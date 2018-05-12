a04bba90eee14f098b23bca79cb8a2ae207218af
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-05-12 11:48:20 +0100

    update gazeBehavior to fix bug

--- python/gazeBehaviour.py
+++ python/gazeBehaviour.py
@@ -6,43 +6,32 @@ class gazeBehaviour:
     def __init__(self, outlet=None):
         pass
 
-    def record(self, timestamp, markers, allBalls, faces, fixation, labels):
+    def record(self, timestamp, allBalls, faces, fixation, labels):
         epsilon = 30  # the threshold in pixels allowed
 
         for ball in allBalls:
-            distX = fixation[0] - ball[0][0][0]
-            distY = fixation[1] - ball[0][0][1]
-
-            if math.sqrt(pow(distX, 2) + pow(distY, 2)) < epsilon:
-                #           time        color of ball    x           y
-                mysample = [timestamp, ball[1], ball[0][0][0], ball[0][0][1], ]
-                return mysample
-
-            for marker in markers:
-
-                cX = marker[0]
-                cY = marker[1]
-
-                distX = fixation[0] - cX
-                distY = fixation[1] - cY
+            if (len(ball[0]) == 1):
+                distX = fixation[0] - ball[0][0][0]
+                distY = fixation[1] - ball[0][0][1]
 
                 if math.sqrt(pow(distX, 2) + pow(distY, 2)) < epsilon:
-                    mysample = [timestamp, 1.0, marker[0], marker[1]]
+                    #           time        color of ball    x           y
+                    mysample = [timestamp, ball[1], ball[0][0][0], ball[0][0][1], ]
                     return mysample
 
-            i = 0
-            for face in faces:
+                i = 0
+                for face in faces:
 
-                cX = face[0]
-                cY = face[1]
-                cW = face[0] + face[2]
-                cH = face[1] + face[3]
+                    cX = face[0]
+                    cY = face[1]
+                    cW = face[0] + face[2]
+                    cH = face[1] + face[3]
 
-                if cX - 30 < fixation[0] < cW + 30 and cY - 30 < fixation[1] < cH + 30:
-                    mysample = [timestamp, 1+i, face[0], face[1]]
-                    return mysample
+                    if cX - 30 < fixation[0] < cW + 30 and cY - 30 < fixation[1] < cH + 30:
+                        mysample = [timestamp, 4+i, face[0], face[1]]
+                        return mysample
 
-                i = i+1
-            mysample = [timestamp, 4.0, 0.0, 0.0]
+                    i = i+1
+            mysample = []
         return mysample
 
