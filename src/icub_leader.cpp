a10592157667cb52f005265ac2953c54d4a9f29b
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-05-21 17:58:09 +0100

    remove unnecessary code from the leader action

--- src/icub_leader.cpp
+++ src/icub_leader.cpp
@@ -24,7 +24,6 @@
 #include <fstream>
 
 #include "helpers.h"
-#include "CvHMM.h"
 
 #define NBSAMPLES 1
 
@@ -72,31 +71,6 @@ class ControlThread: public RateThread
     double v_mag;
     double acc_mag;
     double mag_e; 
-
-    // define matrixes for follower and leader
-    CvHMM hmmFG;
-    double *TRANSdataFG;
-    cv::Mat TRANSFG;
-    double *EMISdataFG;
-    cv::Mat EMISFG;
-    cv::Mat INITFG;
-
-    CvHMM hmmFP;
-    double *TRANSdataFP;
-    cv::Mat TRANSFP;
-    double *EMISdataFP;
-    cv::Mat EMISFP;
-    cv::Mat INITFP;
-
-    // define the sequence of states
-    cv::Mat seq_mat;
-
-    // define the forward and backward probability matrix that are use to calculate the states
-    cv::Mat forward;
-    cv::Mat backward;
-
-    // output from decode
-    cv::Mat pstates;
 
 public:
     ControlThread(int period):RateThread(period){}
@@ -277,123 +251,6 @@ public:
         xf[0] = -0.4;
         xf[1] = -0.05;
         xf[2] =  0.15;
-
-        // initialize the act_probability
-        yInfo() << "1";
-        act_probability = cv::Mat(2,1, CV_64F);
-        yInfo() << "1";
-        act_probability.at<double>(0,0) = 0.5;
-        act_probability.at<double>(1,0) = 0.5;
-
-        /*------------------------ FOLOWER GIVING MODEL ------------------------*/
-        TRANSdataFG = new double[16];        
-        TRANSdataFG[0] = 0.968314321926489;
-        TRANSdataFG[1] = 0.0209125475285171;
-        TRANSdataFG[2] = 0.00697084917617237; 
-        TRANSdataFG[3] = 0.00380228136882129;
-        TRANSdataFG[4] = 0.00638297872340426;
-        TRANSdataFG[5] = 0.979574468085106;
-        TRANSdataFG[6] = 0.00297872340425532;
-        TRANSdataFG[7] = 0.0110638297872340; 
-        TRANSdataFG[8] = 0.0436507936507937; 
-        TRANSdataFG[9] = 0.0198412698412698;
-        TRANSdataFG[10] = 0.922619047619048;
-        TRANSdataFG[11] = 0.0138888888888889;
-        TRANSdataFG[12] = 0.00295159386068477;
-        TRANSdataFG[13] = 0.00118063754427391;
-        TRANSdataFG[14] = 0.00118063754427391;
-        TRANSdataFG[15] = 0.994687131050767;
-
-        TRANSFG = cv::Mat(4,4,CV_64F,TRANSdataFG).clone();  
-
-        EMISdataFG = new double[24];
-        EMISdataFG[0] = 0.699680511182109;
-        EMISdataFG[1] = 0.0396166134185304;
-        EMISdataFG[2] = 0.122683706070288;
-        EMISdataFG[3] = 0.0217252396166134;
-        EMISdataFG[4] = 0.0319488817891374;
-        EMISdataFG[5] = 0.0843450479233227;
-        EMISdataFG[6] = 0.302240512117055;
-        EMISdataFG[7] = 0.186099679926840;
-        EMISdataFG[8] = 0.362597165066301;  
-        EMISdataFG[9] = 0.0214906264288980;
-        EMISdataFG[10] = 0.0832190214906264;
-        EMISdataFG[11] = 0.0443529949702789;
-        EMISdataFG[12] = 0.619918699186992;
-        EMISdataFG[13] = 0.0752032520325203;
-        EMISdataFG[14] = 0.0894308943089431;
-        EMISdataFG[15] = 0.0182926829268293;
-        EMISdataFG[16] = 0.0406504065040650;
-        EMISdataFG[17] = 0.156504065040650;
-        EMISdataFG[18] = 0.181818181818182;
-        EMISdataFG[19] = 0.183175033921303;
-        EMISdataFG[20] = 0.244911804613297;
-        EMISdataFG[21] = 0.0135685210312076;
-        EMISdataFG[22] = 0.350746268656716;
-        EMISdataFG[23] = 0.0257801899592944;
-
-        EMISFG = cv::Mat(4,6,CV_64F,EMISdataFG).clone();
-
-        double INITdataFG[] = {1.0, 0.0, 0.0, 0.0};
-        INITFG = cv::Mat(1,6,CV_64F,INITdataFG).clone();
-
-        std::cout << "FG:";
-        hmmFG.printModel(TRANSFG,EMISFG,INITFG);
-
-        /*------------------------ FOLOWER PLACING MODEL ------------------------*/
-        TRANSdataFP = new double[16];
-        TRANSdataFP[0] = 0.968184653774173;
-        TRANSdataFP[1] = 0.0143480973175296;       
-        TRANSdataFP[2] = 0.0162195882719900;       
-        TRANSdataFP[3] = 0.00124766063630692;       
-        TRANSdataFP[4] = 0.00898410504492053;       
-        TRANSdataFP[5] = 0.979267449896337;       
-        TRANSdataFP[6] = 0.0103662750518314;       
-        TRANSdataFP[7] = 0.00138217000691085;       
-        TRANSdataFP[8] = 0.0258843830888697;       
-        TRANSdataFP[9] = 0.0120793787748059;       
-        TRANSdataFP[10] = 0.958584987057809;       
-        TRANSdataFP[11] = 0.00345125107851596;       
-        TRANSdataFP[12] = 0.0179640718562874;       
-        TRANSdataFP[13] = 0.00598802395209581;       
-        TRANSdataFP[14] = 0.0419161676646707;       
-        TRANSdataFP[15] = 0.934131736526946;       
-
-        TRANSFP = cv::Mat(4,4,CV_64F,TRANSdataFP).clone();
-
-        EMISdataFP = new double[24];        
-        EMISdataFP[0] = 0.754639175257732;
-        EMISdataFP[1] = 0.0130584192439863;
-        EMISdataFP[2] = 0.0192439862542955;
-        EMISdataFP[3] = 0.0219931271477663;
-        EMISdataFP[4] = 0.0371134020618557;
-        EMISdataFP[5] = 0.153951890034364;
-        EMISdataFP[6] = 0.427316293929713;
-        EMISdataFP[7] = 0.0479233226837061;
-        EMISdataFP[8] = 0.00319488817891374;
-        EMISdataFP[9] = 0.0359424920127796;
-        EMISdataFP[10] = 0.159744408945687;
-        EMISdataFP[11] = 0.325878594249201;
-        EMISdataFP[12] = 0.557013118062563,
-        EMISdataFP[13] = 0.0575176589303734;
-        EMISdataFP[14] = 0.0171543895055499;
-        EMISdataFP[15] = 0.0181634712411705;
-        EMISdataFP[16] = 0.0171543895055499;
-        EMISdataFP[17] = 0.332996972754793;
-        EMISdataFP[18] = 0.725000000000000;
-        EMISdataFP[19] = 0.00312500000000000;
-        EMISdataFP[20] = 0.153125000000000;
-        EMISdataFP[21] = 0.00625000000000000;
-        EMISdataFP[22] = 0.0562500000000000;
-        EMISdataFP[23] = 0.0562500000000000;
-
-        EMISFP = cv::Mat(4,6,CV_64F,EMISdataFP).clone();
-
-        double INITdataFP[] = {1.0, 0.0, 0.0, 0.0};
-        INITFP = cv::Mat(1,6,CV_64F,INITdataFP).clone();
-
-        std::cout << "FP:";
-        hmmFP.printModel(TRANSFP,EMISFP,INITFP);
 
         // get arm in initial position
         x[1] =  -0.5; // to the left
@@ -888,158 +745,6 @@ public:
         }
     }
 
-
-    int predictAL(cv::Mat& act_prob, int cur_state, int cur_action){
-        /***********************************************************************************
-        act_prob is a 2x1 matrix with two probabilities, 
-            <0,0> is probability for giving, and 
-            <1,0> i probability for placing action. They sum to 1.
-        This value (as argument) containes the current probabilities 
-            for the two actions that are updated within the function.
-
-        cur_state is 0-5 integer representing the loeaders focuse of attention. 
-            0 - brick 
-            1 - TM face 
-            2 - TM hand 
-            3 - Own hand 
-            4 - TM tower 
-            5 - Own tower
-
-        cur_action //0-giving 1-placing -1-uncertain
-
-        internal parameters of the fucntion
-
-        APdataL - action prediction matrix that is obtained from the data, 
-            it containtes probabilities for each of the action with respect 
-            to the focus of attention of the LEADER
-        rows are foc of leader, 
-        columns are giving and placing probabilities
-        treshold - is a parameter defining the required difference 
-            between the two elements in act_prob to classify the action 
-            as 0-giving 1-placing -1-uncertain
-        alfa - a parameter for moving average filtering a = (1-alfa)*a+alfa*new_val;
-        *************************************************************************************/
-        double APdataL[] = {0.495420313454101, 0.504579686545899,
-                          0.840179238237491, 0.159820761762509,
-                          0.931922196796339, 0.0680778032036613,
-                          0.520000000000000, 0.480000000000000,
-                          0.748014440433213, 0.251985559566787,
-                          0.186659772492244, 0.813340227507756};
-        cv::Mat APL = cv::Mat(6,2,CV_64F,APdataL).clone();
-
-        int new_action;//0-giving 1-placing -1-uncertain
-
-        double rn = rand();
-
-        double prob_G = 0.5;
-        double prob_P = 0.5;
-        double deltalgc = 0.0;
-        double deltalpc = 0.0;
-        double treshold = 0.15;
-        double alfa = 0.01;//parameter of exponential moving average
-
-        if(cur_state>=0 && cur_state<6)
-        {
-            prob_G = APL.at<double>(cur_state,0);
-            prob_P = APL.at<double>(cur_state,1);
-        }
-
-
-        if(rn<prob_G)//it is GIVING
-        {
-            deltalgc = prob_G - act_prob.at<double>(0,0);
-            deltalpc = -deltalgc;
-        }
-        else//it is placing
-        {
-            deltalpc = prob_P - act_prob.at<double>(1,0);
-            deltalgc = -deltalpc;
-        }
-
-        deltalgc = act_prob.at<double>(0,0)+deltalgc;
-        deltalpc = act_prob.at<double>(1,0)+deltalpc;
-
-        //expoential moving average
-        act_prob.at<double>(0,0)=(1-alfa)*act_prob.at<double>(0,0) + alfa*deltalgc;
-        act_prob.at<double>(1,0)=(1-alfa)*act_prob.at<double>(1,0) + alfa*deltalpc;
-
-        //act_prob.at<double>(0,0)=act_prob.at<double>(0,0)+0.1;
-        //act_prob.at<double>(1,0)=act_prob.at<double>(1,0)-0.1;
-
-        if((act_prob.at<double>(0,0)-act_prob.at<double>(1,0))>treshold)
-            new_action = 0;//
-        else if((act_prob.at<double>(0,0)-act_prob.at<double>(1,0))<-treshold)
-            new_action = 1;
-        else
-            new_action = -1;
-
-        return new_action;
-    }
-
-    /***************************************************/
-    void actionBehavior(int state)
-    {
-        // input: state of the human
-        //        logpseg -- irrelevant for now
-        // output: behavior of robot
-        int action;
-        double logpseq;
-
-        // call the predictor function
-        action = predictAL(act_probability, state, action);
-        //yInfo() << act_probability.at<double>(0,0);
-        //yInfo() << act_probability.at<double>(1,0);
-
-        // add state to sequence of states
-        seq_mat.push_back(state);
-
-        // make a decision based on the predictor's response
-        if (action == 0){
-            // get current location
-            iarm->getPose(p,o);
-            iarm->setPosePriority("orientation");
-            Vector od = o;
-            // get current velocities
-            iarm->getTaskVelocities(vcur, wcur);
-
-            od = computeHandOrientationPassing(_hand); //get default orientation
-            reachArmGiving(p, od, xf, vcur);
-
-            hmmFG.decodeMR2(seq_mat,TRANSFG,EMISFG,INITFG,logpseq,pstates,forward,backward);
-            gazeBehavior(pstates);
-
-        } else if (action == 1) {
-            // just observe the action
-            yInfo() << "I'm observing";
-
-            iarm->getPose(p,o);
-            // get current velocities
-            iarm->getTaskVelocities(vcur, wcur);
-
-            //closing back the hand
-            moveFingers(_hand,thumb,1.0);
-            moveFingers(_hand,fingers,0.5);
-            // go back to start position
-            x[1] =  -0.5; // to the left
-            startingArm(x);
-
-            hmmFP.decodeMR2(seq_mat,TRANSFP,EMISFP,INITFP,logpseq,pstates,forward,backward);
-            gazeBehavior(pstates);
-
-            count++; // count the number of times is giving
-            // alternative is getting the percentage of giving and when it is higher than
-            // 70% you can close the hand
-
-        } else {
-            yInfo() << "Wrong action";
-            //cout << "M = "<< endl << " "  << seq_mat << endl << endl;
-            hmmFP.decodeMR2(seq_mat,TRANSFP,EMISFP,INITFP,logpseq,pstates,forward,backward);
-            //cout << "M = "<< endl << " "  << pstates << endl << endl;
-            //getchar();
-            gazeBehavior(pstates);
-        }
-    }
-
     void reachArmGiving(Vector desired_p, Vector orientation, Vector x_pos, Vector velocity)
     {
         e[0] = x_pos[0] - desired_p[0];
@@ -1104,73 +809,151 @@ public:
     }
 
     /***************************************************/
-    void gazeBehavior(cv::Mat &state)
+    void release(string hand)
     {
-        // check all the state and take the highest probability one
-        double max = 0;   
-        int id = -1;
+        IControlLimits2   *ilim;
+        IPositionControl2 *ipos;
+        IControlMode2     *imod;
 
-        int nRows = state.rows;
-        int nCols = state.cols;
-        cout << "MaxStates = "<< endl << " "  << state << endl << endl;
-
-        // we only want the last column
-        int i,j = nCols - 1;
-        double *p;
-        for( i = 0; i < nRows; ++i)
-        {   
-            // get the max probability and the state index
-            p = state.ptr<double>(i);
-            yInfo() << p[j];
-            if (max < p[j]) {
-                max = p[j];
-                id = i;
-            }
+        if (hand=="right")
+        {
+            drvHandR.view(ilim);
+            drvHandR.view(ipos);
+            drvHandR.view(imod);
         }
-        // sending the highest probable state to the robot
-        fixate(id+1);
+        else
+        {
+            drvHandL.view(ilim);
+            drvHandL.view(ipos);
+            drvHandL.view(imod);
+        }
+
+        double target[16] = {0, 90, 0, 20, 10, 0, 0, 50, 10, 0, 0, 0, 0, 0, 0, 0};   //icubSim
+        //double target[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};   // icub
+
+        // we set up here the lists of joints we need to actuate (just hand)
+        // shoulders (3) + elbow + wrist (3) + hand openning + thumb (3)
+        VectorOf<int> joints;
+        for (int i=0; i<16; i++){
+            joints.push_back(i);
+        }
+
+        // This option you will move each joint individually
+        for (size_t i=7; i<joints.size(); i++)
+        {
+            int j=joints[i];
+            // retrieve joint bounds
+            double min_j,max_j,range;
+            ilim->getLimits(j,&min_j,&max_j);
+            // set up the speed in [deg/s]
+            ipos->setRefSpeed(j,15.0);
+            // set up max acceleration in [deg/s^2]
+            ipos->setRefAcceleration(j,5.0);
+            // yield the actual movement
+            yInfo()<<"Yielding new target: "<<target[i]<<" [deg]";
+            imod->setControlMode(j,VOCAB_CM_POSITION);
+            ipos->positionMove(j,target[i]);
+        }
+        // wait (with timeout) until the movement is completed
+        bool done=false;
+        double t0=Time::now();
+        while (!done && (Time::now()-t0 < 1.0))
+        {
+            yInfo()<<"Waiting...";
+            Time::delay(0.1);   // release the quantum to avoid starving resources
+            ipos->checkMotionDone(&done);
+        }
+
+        if (done)
+            yInfo()<<"Movement completed";
+        else
+            yWarning()<<"Timeout expired";
+        // wait until all fingers have attained their set-points
     }
+
+    std::vector< std::vector<float> > loadDataFile(std::string file, bool convert = false)
+    {
+        // Load the dataset from a file
+        float valTmp;
+        char tmp[1024];
+        unsigned int l=0, c=0;
+        // initialize the vector size
+        std::vector< std::vector<float> > result(
+                1001,
+                std::vector<float>(4)); 
+
+        yInfo() << "loading file: " << file;
+        std::ifstream f(file.c_str());
+
+        if (f.is_open()){
+            // Get number of rows
+            while(!f.eof()){ 
+                f.getline(tmp,1024);
+                l++;
+                if (l==1)
+                {
+                    // Get number of columns
+                    std::istringstream strm;
+                    strm.str(tmp); 
+                    while (strm >> valTmp)
+                    c++;
+                }
+            }
+            l--;
+            f.clear();
+            f.seekg(0); // returns to beginning of the file
+
+            // for head and arm data
+            if (not convert) {
+                for(unsigned int i=0;i<l;i++){ 
+                    f.getline(tmp,1024);
+                    std::istringstream strm;
+                    strm.str(tmp); 
+                    for(unsigned int j=0;j<c;j++){
+                        strm >> result[i][j];
+                        //printf("%f ",result[i][j]);
+                    }
+                    //printf("\n");
+                }
+            // for eyes data
+            } else {
+                for(unsigned int i=0;i<l;i++){ 
+                    f.getline(tmp,1024);
+                    std::istringstream strm;
+                    strm.str(tmp); 
+                    for(unsigned int j=0;j<c;j++){
+                        strm >> result[i][j];
+                        if (j == 1){
+                            if (result[i][j] == 1){
+                                result[i][1] = -0.55;
+                                result[i][2] =  0.00;
+                                result[i][3] =  0.45;                    
+                            }else if (result[i][j] == 2){
+                                result[i][1] = -0.35;  //move forward 10 cm
+                                result[i][2] = -0.19;  //move left 20 cm
+                                result[i][3] =  0.02;  //move down 10 cm
+                            }else if (result[i][j] == 3){
+                                result[i][1] = -0.25;
+                                result[i][2] =  0.30;
+                                result[i][3] =  0.45;                
+                            }
+                        }else{
+                            continue;//printf("%f ", float(result[i][j]));
+                        }
+                    }
+                    printf("\n");
+                }
+            }
+        }else{
+            std::cout  << std::endl << "Error opening file " << std::endl; 
+        }
+        f.close();
+        return result;
+    }    
 
     void run()
     {
-      	Vector pupil; string hand; Vector gaze;
-        int state; // state the human is in
-        if (inPort.read(pupil))
-        {
-            if ( pupil(1) == 1){
-                yInfo() << "Teammate's Tower";
-                state = 5;
-           
-            }else if ( pupil(1) == 2) {
-                yInfo() << "My Tower";
-                state = 6;
-                
-            }else if ( pupil(1) == 3) {
-                yInfo() << "Brick/Object";
-                state = 1;
-  
-            }else if ( pupil(1) == 4) {
-                yInfo() << "iCub's Face";
-                state = 2;
-
-            }else if ( pupil(1) == 5) {
-                yInfo() << "iCub's Hand";
-                state = 3;
-
-            }else if ( pupil(1) == 4) {
-                yInfo() << "Own's Hand";
-                state = 4;
-                
-            } else {
-                yInfo() << "wrong state";
-                state = -1;            
-            }
-        
-            if (state != -1){
-                // if you observe the human looking at one of the states then act
-                actionBehavior(state);
-            }
-        }
+      	
     }
 };
 
